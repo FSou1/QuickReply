050066bfc5ed96065c64f4398b9af85b4fcf00ea
FSou1 <my.fsou1@gmail.com>
2020-07-04 17:59:10 -0500

    Add list-view/tile-view to reply-list

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_CONFIG, APP_DI_CONFIG } from './app.config';
+import { SortablejsModule } from 'ngx-sortablejs';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SortablejsModule.forRoot({
+      animation: 300,
+    })
+  ],
   bootstrap: [AppComponent],
   providers: [{ provide: APP_CONFIG, useValue: APP_DI_CONFIG }]
 })
